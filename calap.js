13cc14ca65029ca213cf116afffdba773d8bea0f
20252MCA0210VATTIKUTI VENKATA ADARSH <20252MCA0210@presidencyuniversity.in>
2026-08-17 15:23:21 +0530

    initial commit

---
+++ calap.js
@@ -0,0 +1,4 @@
+const {add,sub}=require('./calculator1');
+
+console.log(add(10,20));
+console.log(sub(100,20));
---
+++ calculator1.js
@@ -0,0 +1,7 @@
+function add(a, b){
+    return a + b;
+}
+function sub(c,d){
+    return c - d;
+}
+module.exports ={add, sub}; 
